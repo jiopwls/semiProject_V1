73c34ae0c806884a99909e2f69886a8eccb65e25
jiopwls <jiopwls@naver.com>
2022-09-29 10:05:32 +0900

    requestmapping 설정

--- src/main/java/jinn/spring/mvc/controller/BoardController.java
+++ src/main/java/jinn/spring/mvc/controller/BoardController.java
@@ -1,5 +1,24 @@
 package jinn.spring.mvc.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class BoardController {
 
+	@GetMapping("/board")
+	public String join() {
+		return "board/list";
+	}
+	
+	@GetMapping("/board")
+	public String login() {
+		return "board/view";
+	}
+	
+	@GetMapping("/board")
+	public String myinfo() {
+		return "board/write";
+	}
+	
 }
