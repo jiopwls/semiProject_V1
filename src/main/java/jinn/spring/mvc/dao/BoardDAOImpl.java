bd953d4b3de6c730c98b729040f49c56248ed8a0
jiopwls <jiopwls@naver.com>
2022-09-30 12:40:18 +0900

    insert 쿼리 문을 심플한 쿼리문으로 바꿈

--- src/main/java/jinn/spring/mvc/dao/BoardDAOImpl.java
+++ src/main/java/jinn/spring/mvc/dao/BoardDAOImpl.java
@@ -4,6 +4,8 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
@@ -13,20 +15,30 @@ import jinn.spring.mvc.vo.BoardVO;
 public class BoardDAOImpl implements BoardDAO{
 
 	
-	@Autowired
+	//@Autowired log4j.xml bean 태그에 정의한 경우 생략가능
 	private JdbcTemplate jdbcTemplate;
 	private SimpleJdbcInsert simpleInsert;
 	
-	
+	public BoardDAOImpl(DataSource dataSource) {
+		simpleInsert = new SimpleJdbcInsert(dataSource)
+				.withTableName("board")
+				.usingColumns("title","userid","contents");
+	}
 	
 	@Override
 	public int insertBoard(BoardVO bvo) {
-		String sql = "insert into board" + "(title,userid,contents)" + "value(?,?,?)";
+		SqlParameterSource params = new BeanPropertySqlParameterSource(bvo);
 		
-		Object[] params = new Object[] {
-				bvo.getTitle(), bvo.getUserid(), bvo.getContents()
-		};
-		return jdbcTemplate.update(sql, params);
+		return simpleInsert.execute(params);
 	}
+//	@Override
+//	public int insertBoard(BoardVO bvo) {
+//		String sql = "insert into board" + "(title,userid,contents)" + "value(?,?,?)";
+//		
+//		Object[] params = new Object[] {
+//				bvo.getTitle(), bvo.getUserid(), bvo.getContents()
+//		};
+//		return jdbcTemplate.update(sql, params);
+//	}
 
 }
--- src/main/java/jinn/spring/mvc/dao/MemberDAOImpl.java
+++ src/main/java/jinn/spring/mvc/dao/MemberDAOImpl.java
@@ -1,25 +1,45 @@
 package jinn.spring.mvc.dao;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
+import jinn.spring.mvc.vo.BoardVO;
 import jinn.spring.mvc.vo.MemberVO;
 
 @Repository("mdao")
 public class MemberDAOImpl implements MemberDAO{
 
-	@Autowired
+	// @Autowired log4j.xml bean 태그에 정의한 경우 생략가능
 	private JdbcTemplate jdbcTemplate;
+	private SimpleJdbcInsert simpleInsert;
+	
+	public MemberDAOImpl(DataSource dataSource) {
+		simpleInsert = new SimpleJdbcInsert(dataSource)
+				.withTableName("member")
+				.usingColumns("userid","passwd","name","email");
+	}
 	
 	@Override
 	public int insertMember(MemberVO mvo) {
+		SqlParameterSource params = new BeanPropertySqlParameterSource(mvo);
 		
-		String sql = "insert into member" + "(userid,passwd,name,email)" + " value(?,?,?,?)";
-		
-		Object[] params = new Object[] {
-				mvo.getUserid(), mvo.getPasswd(), mvo.getName(), mvo.getEmail()};
-		return jdbcTemplate.update(sql, params);
+		return simpleInsert.execute(params);
 	}
+	
+//	@Override
+//	public int insertMember(MemberVO mvo) {
+//		
+//		String sql = "insert into member" + "(userid,passwd,name,email)" + " value(?,?,?,?)";
+//		
+//		Object[] params = new Object[] {
+//				mvo.getUserid(), mvo.getPasswd(), mvo.getName(), mvo.getEmail()};
+//		return jdbcTemplate.update(sql, params);
+//	}
 
 }
