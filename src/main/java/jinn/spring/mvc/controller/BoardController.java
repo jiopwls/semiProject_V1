45b0eed253594ade6a03d48042e34f2b000c955f
jiopwls <jiopwls@naver.com>
2022-10-05 11:11:06 +0900

    list 페이지네이션 구현1

--- src/main/java/jinn/spring/mvc/controller/BoardController.java
+++ src/main/java/jinn/spring/mvc/controller/BoardController.java
@@ -28,9 +28,8 @@ public class BoardController {
 	 총 페이지 수 : 전체 게시물 수 / 페이지 당 게시물 수
 	 총 페이지 수 pages : ceil(getTotalPage / perPage)
 	 예) 2 = 50/25, 3 = 51/25  
-	 */
 	
-	/*
+	
 	 페이지 별 읽어올 게시글 범위
 	 총 게시글이 55건이라 할때
 	 1page : 1 ~ 25
@@ -38,16 +37,34 @@ public class BoardController {
 	 3page : 51 ~ 55
 	 ipage : m번째 ~ n번째까지
 	 m = ( i - 1 ) * 25 + 1  
+	
+	 
+	 현재 페이지에 따라 보여줄 페이지 블럭 결정
+	 ex) 총 페이지 수가 27일 때
+	 cpg = 1 -> 1 2 3 4 5 6 7 8 9 10
+	 cpg = 4 -> 1 2 3 4 5 6 7 8 9 10
+	 cpg = 10 -> 1 2 3 4 5 6 7 8 9 10
+	 cpg = 11 -> 11 12 13 14 15 16 17 18 19 20
+	 cpg = 26 -> 21 22 23 24 25 26 27
+	 
+	 cpg = n : ?+0 ?+1 ?+2 .... ?+9
+	 stpgn = ((cpg - 1) / 10) * 10 + 1
 	 
 	 */
 	
 	@GetMapping("/list")
 	public String list(Model m, String cpg) {
+		
 		int perPage = 20;
-		int snum = (Integer.parseInt(cpg) - 1) * perPage;
+		if (cpg == null || cpg.equals("")) cpg = "1";
+		
+		int cpage = Integer.parseInt(cpg);
+		int snum = ( cpage- 1) * perPage;
+		int stpgn = ((cpage - 1) / 10) * 10 + 1;
 		
 		m.addAttribute("bdlist", bsrv.readBoard(snum));
-		
+		m.addAttribute("stpgn", stpgn);
+		//m.addAttribute("cpg", cpage);
 		
 		return "board/list";
 	}
