a4254bdebbd83f9434e410382a53c804f76075e0
jiopwls <jiopwls@naver.com>
2022-10-07 14:55:45 +0900

    lombok 라이브러리 설치

--- src/main/java/jinn/spring/mvc/vo/Zipcode.java
+++ src/main/java/jinn/spring/mvc/vo/Zipcode.java
@@ -1,5 +1,12 @@
 package jinn.spring.mvc.vo;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@ToString
 public class Zipcode {
 	private String zipcode;
 	private String sido;
@@ -7,53 +14,6 @@ public class Zipcode {
 	private String dong;
 	private String ri;
 	private String bunji;
-	private String seq;
-	
-	public Zipcode() {
-		
-	}
-	
-	public String getZipcode() {
-		return zipcode;
-	}
-	public void setZipcode(String zipcode) {
-		this.zipcode = zipcode;
-	}
-	public String getSido() {
-		return sido;
-	}
-	public void setSido(String sido) {
-		this.sido = sido;
-	}
-	public String getGugun() {
-		return gugun;
-	}
-	public void setGugun(String gugun) {
-		this.gugun = gugun;
-	}
-	public String getDong() {
-		return dong;
-	}
-	public void setDong(String dong) {
-		this.dong = dong;
-	}
-	public String getRi() {
-		return ri;
-	}
-	public void setRi(String ri) {
-		this.ri = ri;
-	}
-	public String getBunji() {
-		return bunji;
-	}
-	public void setBunji(String bunji) {
-		this.bunji = bunji;
-	}
-	public String getSeq() {
-		return seq;
-	}
-	public void setSeq(String seq) {
-		this.seq = seq;
-	}
+	private String seq;	
 	
 }
